2196242782372087073dfae19455a61f82a02997
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-11-04 13:23:28 +0700

    :newspaper: setup screen

---
+++ src/screens/home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, SafeAreaView, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+    return <SafeAreaView>
+        <View>
+            <Text>Home</Text>
+        </View>
+    </SafeAreaView>
+}
