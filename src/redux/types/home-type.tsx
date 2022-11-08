f8ff6600a72bbeb59e14588b0404c48a8389484c
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-11-08 16:17:38 +0700

    :newspaper: home action type

---
+++ src/redux/types/home-type.tsx
@@ -0,0 +1,7 @@
+export const HOME_LOAD_DATA = "HOME_LOAD_DATA";
+
+interface homeLoadData {
+    type: typeof HOME_LOAD_DATA;
+}
+
+export type HomeActionType = homeLoadData;
--- src/redux/types/index.tsx
+++ src/redux/types/index.tsx
@@ -1,1 +1,2 @@
 export * from './splash-type';
+export * from './home-type';
