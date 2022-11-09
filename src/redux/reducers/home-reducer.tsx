301fe251327a8050064185cd07c6a3563f7735e8
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-11-09 13:47:46 +0700

    :pencil: update interface reducer

--- src/redux/reducers/home-reducer.tsx
+++ src/redux/reducers/home-reducer.tsx
@@ -1,18 +1,22 @@
 import { UserModel } from "../../models/user-model";
 import { HomeActionType, HOME_LOAD_DATA } from "../types";
 
+interface HomeState {
+    users: UserModel[],
+}
+
 const initialState = {
     users: [],
 }
 
-export function homeReducer(state = initialState, action: HomeActionType) {
+export function homeReducer(state = initialState, action: HomeActionType): HomeState {
     switch (action.type) {
         case HOME_LOAD_DATA:
             return {
                 ...state,
                 users: action.payload,
             }
-            default: 
+        default: 
             return state;
     }
 }
--- src/redux/reducers/splash-reducer.tsx
+++ src/redux/reducers/splash-reducer.tsx
@@ -1,10 +1,14 @@
 import { SplashActionType, SPLASH_INIT_DATA_START, SPLASH_INIT_DATA_SUCCESS } from "../types";
 
+interface SplashState {
+    isSuccess: boolean,
+}
+
 const initialState = {
     isSuccess: false,
 }
 
-export function splashReducer(state = initialState, action: SplashActionType) {
+export function splashReducer(state = initialState, action: SplashActionType): SplashState {
     switch (action.type) {
         case SPLASH_INIT_DATA_START: 
             return {
