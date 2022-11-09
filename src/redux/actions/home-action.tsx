8eed9c3c9e3db62fa6febc93803282752c24c177
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-11-09 13:54:42 +0700

    :pencil: home load realm objects

--- src/redux/actions/home-action.tsx
+++ src/redux/actions/home-action.tsx
@@ -1,4 +1,5 @@
 import { ActionCreator } from "redux";
+import { initRealm } from "../../database/database-realm";
 import { UserModel } from "../../models/user-model";
 import { HomeActionType, HOME_LOAD_DATA } from "../types";
 
@@ -9,4 +10,15 @@ const homeLoadData: ActionCreator<HomeActionType> = (
         type: HOME_LOAD_DATA,
         payload: users,
     }
+}
+
+export function loadFromRealm() {
+    return async (dispatch: (arg0: HomeActionType) => void) => {
+        const dbRealm = await initRealm();
+        const rawUsers = dbRealm.objects("users")
+        console.log(`Home Action # data users`);
+        console.log(rawUsers);
+        dispatch(homeLoadData([]));
+    }
+    
 }
