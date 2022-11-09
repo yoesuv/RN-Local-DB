fc31c112b5c77c8af71b0902e7a450964e3bfc16
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-11-09 15:18:44 +0700

    :sparkles: show list user

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { StyleSheet, SafeAreaView, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, View, FlatList, VirtualizedList } from 'react-native';
 import { RootState } from '../redux/reducers';
 import { loadFromRealm } from '../redux/actions';
+import { UserModel } from '../models/user-model';
 
 export default function HomeScreen() {
 
@@ -16,9 +17,37 @@ export default function HomeScreen() {
         }
     },[stateHome.isSuccess])
 
-    return <SafeAreaView>
-        <View>
-            <Text>Home</Text>
+    interface TIItemUser {
+        user: UserModel
+    }
+
+    const itemUser = ({user}: TIItemUser)  => {
+        return <View style={styles.itemUser}>
+            <Text style={styles.label}>{user.name}</Text>
         </View>
+    }
+
+    return <SafeAreaView style={styles.container}>
+        <FlatList 
+            data={stateHome.users}
+            renderItem={({item}) => itemUser({user: item}) } 
+            keyExtractor={(item, index) => index.toString()}
+        />
     </SafeAreaView>
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+    itemUser: {
+        padding: 12,
+        borderBottomColor: '#F5F5F5',
+        borderBottomWidth: 1, 
+    },
+    label: {
+        fontSize: 16,
+        color: 'black',
+    }
+})
