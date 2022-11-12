fb1606766752b89d2d8aa37c325eada6585dae0f
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-11-12 20:52:47 +0700

    :pencil: add action load detail failed

--- src/redux/actions/detail-action.tsx
+++ src/redux/actions/detail-action.tsx
@@ -2,7 +2,7 @@ import { ActionCreator } from "redux";
 import { TB_USERS } from "../../data/constants";
 import { initRealm } from "../../database/database-realm";
 import { UserModel } from "../../models/user-model";
-import { DetailActionType, DETAIL_LOAD_DATA } from "../types";
+import { DetailActionType, DETAIL_LOAD_DATA, DETAIL_LOAD_FAILED } from "../types";
 
 const detailLoadData: ActionCreator<DetailActionType> = (
     user: UserModel
@@ -13,13 +13,22 @@ const detailLoadData: ActionCreator<DetailActionType> = (
     }
 }
 
+const detailLoadFailed: ActionCreator<DetailActionType> = () => {
+    return {
+        type: DETAIL_LOAD_FAILED,
+    }
+}
+
 export function loadUser(id: number) {
     return async (dispatch: (arg0: DetailActionType) => void) => {
-        const dbRealm = await initRealm();
-        const rawUsers = dbRealm.objects<UserModel[]>(TB_USERS);
-        const selectedUser = rawUsers.filtered(`id = ${id}`)[0];
-        console.log('load user id', JSON.stringify(selectedUser));
-        console.log(selectedUser);
-        dispatch(detailLoadData(selectedUser));
+        try {
+            const dbRealm = await initRealm();
+            const rawUsers = dbRealm.objects<UserModel[]>(TB_USERS);
+            const selectedUser = rawUsers.filtered(`id = ${id}`)[0];
+            console.log('load user id', JSON.stringify(selectedUser));
+            dispatch(detailLoadData(selectedUser));
+        } catch (e) {
+            dispatch(detailLoadFailed());
+        }
     }
 }
