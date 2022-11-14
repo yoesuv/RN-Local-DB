6fe512e7fd11e11fe9819148b258f9412328d3bc
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-11-14 09:08:34 +0700

    :hammer: throw exception

--- src/database/database-realm.tsx
+++ src/database/database-realm.tsx
@@ -4,9 +4,13 @@ import { CompanyModel } from "../models/company-model";
 import { AddressModel } from "../models/address-model";
 import { GeoModel } from "../models/geo-model";
 
-export async function initRealm(): Promise<Realm> {
-     return await Realm.open({
-        path: "rn_local_db",
-        schema: [ UserModel.schema, CompanyModel.schema, AddressModel.schema, GeoModel.schema ],
-    })
+export async function initRealm(): Promise<Realm | never> {
+    try {
+        return await Realm.open({
+            path: "rn_local_db",
+            schema: [ UserModel.schema, CompanyModel.schema, AddressModel.schema, GeoModel.schema ],
+        })
+    } catch (e) {
+        throw e;
+    }
 }
